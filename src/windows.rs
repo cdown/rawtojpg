8f4ced686f5f7d75cf8555180d2bd16650691882
Chris Down <chris@chrisdown.name>
2025-02-23 20:37:42 +0000

    Add Windows support
    This implements the same general strategy as our madvise() strategy on
    Linux. We open the file flagged as random access to avoid readahead, and
    then call PrefetchVirtualMemory on the bit we care about.

---
+++ src/windows.rs
@@ -0,0 +1,36 @@
+use anyhow::Result;
+use memmap2::Mmap;
+use std::os::windows::io::AsRawHandle;
+use std::path::Path;
+use tokio::fs::{File, OpenOptions};
+use windows::Win32::Storage::FileSystem::FILE_FLAG_RANDOM_ACCESS;
+use windows::Win32::System::Memory::{PrefetchVirtualMemory, WIN32_MEMORY_RANGE_ENTRY};
+use windows::Win32::System::Threading::GetCurrentProcess;
+
+pub fn mmap_raw(file: File) -> Result<Mmap> {
+    // SAFETY: see comment in unix.rs
+    let raw_buf = unsafe { Mmap::map(file.as_raw_handle())? };
+    Ok(raw_buf)
+}
+
+pub async fn open_raw(path: &Path) -> Result<File> {
+    // There's no MADV_RANDOM equivalent, we have to do it at open time. See unix.rs for why we do
+    // this in general.
+    Ok(OpenOptions::new()
+        .read(true)
+        .custom_flags(FILE_FLAG_RANDOM_ACCESS.0)
+        .open(path)
+        .await?)
+}
+
+pub fn prefetch_jpeg(raw_buf: &Mmap, jpeg: &crate::EmbeddedJpegInfo) -> Result<()> {
+    unsafe {
+        let process = GetCurrentProcess();
+        let entry = [WIN32_MEMORY_RANGE_ENTRY {
+            VirtualAddress: raw_buf.as_ptr().add(jpeg.offset) as *mut _,
+            NumberOfBytes: jpeg.length,
+        }];
+        PrefetchVirtualMemory(process, &entry, 0)?;
+    }
+    Ok(())
+}
